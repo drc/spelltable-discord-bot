7f1067e53f328d7519ca788b5f456bfef85e3299
Dan Cigrang <danielcigrang@me.com>
2024-03-06 20:24:28 -0600

    new command

--- src/commands.js
+++ src/commands.js
@@ -3,6 +3,18 @@ export const HAD_IT_COMMAND = {
 	description: "When you've just about had it.",
 };
 
+export const RANDOM_COMMAND = {
+	name: "random",
+	description: "Get a random card image",
+	options: [
+		{
+			type: 3,
+			name: "text",
+			description: "tesing input",
+		},
+	],
+};
+
 export const INVITE_COMMAND = {
 	name: "invite",
 	description: "Get an invite link to add the bot to your server",
