99d650e5f612d64961b1d97ed4c1d7ed11114fd2
Dan Cigrang <danielcigrang@me.com>
2024-10-28 21:18:44 -0500

    one checkopoint left, need to do the message interactions

--- src/commands/card.ts
+++ src/commands/card.ts
@@ -1,51 +1,51 @@
 import JsonResponse from "../response";
-import * as dsi from "discord-interactions";
-import * as scry from "./../scryfall.js";
+import { InteractionResponseType, MessageComponentTypes, ButtonStyleTypes } from "discord-interactions";
+import { getRandomUrl, getNamedUrl, getAutoCompleteSets } from "./../scryfall.js";
 
 export default async function handleCardCommand(interaction): Promise<JsonResponse> {
-	const set = interaction?.data?.options[1]?.value ?? null;
-	const cardName = interaction?.data?.options[0]?.value ?? null;
+	const set: String = interaction?.data?.options[1]?.value ?? null;
+	const cardName: String = interaction?.data?.options[0]?.value ?? null;
 	if (set) {
-		handleSetCondition(set, cardName);
+		// break early with the custom card
+		return await handleSetCondition(set, cardName);
 	}
 
 	if (cardName) {
-		handleJustTheCardName(cardName);
+		return await handleJustTheCardName(cardName);
 	}
 
-	const randomUrl = await scry.getRandomUrl();
+	const randomUrl = await getRandomUrl();
 	return new JsonResponse({
-		type: dsi.InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+		type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
 		data: {
 			content: randomUrl,
 		},
 	});
 }
 
-async function handleJustTheCardName(cardName) {
-	const result = await scry.getNamedUrl(cardName);
+async function handleJustTheCardName(cardName: String): Promise<Response> {
+	const result = await getNamedUrl(cardName);
 	if (result) {
 		const { namedUrl, externalUrl } = result;
-		console.log(namedUrl, externalUrl);
 		if (!namedUrl) {
 			return new JsonResponse({
-				type: dsi.InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+				type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
 				data: {
 					content: "# No cards found\nYour search didn't match any cards.",
 				},
 			});
 		}
 		return new JsonResponse({
-			type: dsi.InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+			type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
 			data: {
 				content: namedUrl,
 				components: [
 					{
-						type: dsi.MessageComponentTypes.ACTION_ROW,
+						type: MessageComponentTypes.ACTION_ROW,
 						components: [
 							{
-								type: dsi.MessageComponentTypes.BUTTON,
-								style: dsi.ButtonStyleTypes.LINK,
+								type: MessageComponentTypes.BUTTON,
+								style: ButtonStyleTypes.LINK,
 								label: "View on Scryfall",
 								url: externalUrl,
 							},
@@ -55,26 +55,27 @@ async function handleJustTheCardName(cardName) {
 			},
 		});
 	}
+	return new Response("Card not found", { status: 404 });
 }
 
 async function handleSetCondition(set, cardName) {
-	const result = await scry.getAutoCompleteSets(cardName);
+	const result = await getAutoCompleteSets(cardName);
 	if (result) {
 		const { cardImages } = result;
 		const card = cardImages.filter((card) => card.set === set);
 		if (card.length > 0) {
 			const { url, uri } = card[0];
 			return new JsonResponse({
-				type: dsi.InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+				type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
 				data: {
 					content: url,
 					components: [
 						{
-							type: dsi.MessageComponentTypes.ACTION_ROW,
+							type: MessageComponentTypes.ACTION_ROW,
 							components: [
 								{
-									type: dsi.MessageComponentTypes.BUTTON,
-									style: dsi.ButtonStyleTypes.LINK,
+									type: MessageComponentTypes.BUTTON,
+									style: ButtonStyleTypes.LINK,
 									label: "View on Scryfall",
 									url: uri,
 								},
@@ -85,4 +86,5 @@ async function handleSetCondition(set, cardName) {
 			});
 		}
 	}
+	return new Response("Set not found", { status: 404 });
 }
--- src/worker.js
+++ src/worker.js
@@ -3,10 +3,8 @@
  */
 
 import { Router } from "itty-router";
-import * as dsi from "discord-interactions";
-import * as command from "./commands.js";
-import { getRandomUrl, getNamedUrl, getAutoCompleteNames, getAutoCompleteSets } from "./scryfall.js";
-import * as act from "./interactions.js";
+import { InteractionType, verifyKey } from "discord-interactions";
+import {handleApplicationCommand, handleApplicationAutoComplete}  from "./interactions.js";
 import JsonResponse from "./response.js";
 
 /**
@@ -45,55 +43,21 @@ router.post("/", async (request, env) => {
 	}
 
 	switch (interaction.type) {
-		case dsi.InteractionType.PING: {
+		case InteractionType.PING: {
 			// The `PING` message is used during the initial webhook handshake, and is
 			// required to configure the webhook in the developer portal.
 			return new JsonResponse({
-				type: dsi.InteractionResponseType.PONG,
+				type: InteractionResponseType.PONG,
 			});
 		}
-		case dsi.InteractionType.APPLICATION_COMMAND:
-			return await act.handleApplicationCommand(interaction, env);
-		case dsi.InteractionType.APPLICATION_COMMAND_AUTOCOMPLETE:
-			return await act.handleApplicationAutoComplete(interaction, env);
-		case dsi.InteractionType.MESSAGE_COMPONENT:
+		case InteractionType.APPLICATION_COMMAND:
+			return await handleApplicationCommand(interaction, env);
+		case InteractionType.APPLICATION_COMMAND_AUTOCOMPLETE:
+			return await handleApplicationAutoComplete(interaction, env);
+		case InteractionType.MESSAGE_COMPONENT:
 		default:
 			return new Response("Unsupported interaction type.", { status: 400 });
 	}
-
-	if (interaction.type === dsi.InteractionType.APPLICATION_COMMAND_AUTOCOMPLETE) {
-		// Autocomplete interactions will come as `APPLICATION_COMMAND_AUTOCOMPLETE`.
-		// console.log("Autocomplete", interaction.data);
-		const searcher = interaction.data.options.filter((option) => option.focused)[0].name;
-		switch (searcher) {
-			case command.CARD_COMMAND.options[0].name: {
-				const names = await getAutoCompleteNames(interaction.data?.options[0]?.value);
-				return new JsonResponse({
-					type: dsi.InteractionResponseType.APPLICATION_COMMAND_AUTOCOMPLETE_RESULT,
-					data: {
-						choices: names.map((name) => ({
-							name,
-							value: name,
-						})),
-					},
-				});
-			}
-			case command.CARD_COMMAND.options[1].name: {
-				const { sets: mtgset } = await getAutoCompleteSets(interaction.data?.options[0]?.value);
-				const filteredSet = mtgset.filter((s) => s.set.startsWith(interaction.data?.options[1]?.value));
-				return new JsonResponse({
-					type: dsi.InteractionResponseType.APPLICATION_COMMAND_AUTOCOMPLETE_RESULT,
-					data: {
-						choices: filteredSet.map((s) => ({
-							name: `${s.set.toUpperCase()} (${s.collector_number})`,
-							value: s.set,
-						})),
-					},
-				});
-			}
-		}
-	}
-
 	if (interaction.type === dsi.InteractionType.MESSAGE_COMPONENT) {
 		switch (interaction.data?.custom_id) {
 			case "join_game": {
@@ -226,7 +190,7 @@ async function verifyDiscordRequest(request, env) {
 	const signature = request.headers.get("x-signature-ed25519");
 	const timestamp = request.headers.get("x-signature-timestamp");
 	const body = await request.text();
-	const isValidRequest = signature && timestamp && dsi.verifyKey(body, signature, timestamp, env.DISCORD_PUBLIC_KEY);
+	const isValidRequest = signature && timestamp && verifyKey(body, signature, timestamp, env.DISCORD_PUBLIC_KEY);
 	if (!isValidRequest) {
 		return { isValid: false };
 	}
